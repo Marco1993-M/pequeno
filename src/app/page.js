d9afd9290b1951d5c6211ad345b7aab006b0fd74
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-08-25 13:40:29 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Link from 'next/link';
+import SocialProofStrip from "@/components/SocialProofStrip";
 
 export default function Page() {
   const [showMore, setShowMore] = useState(false);
