287c824e83156d7eea93557e22f53c4eae5daa96
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-09-17 11:46:02 +0200

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="absolute top-0 left-0 w-full z-50 border-b border-gray-200">
+    <nav className="absolute top-0 left-0 w-full z-50">
       <div className="mx-auto flex items-center justify-between px-4 py-4" style={{ maxWidth: "85vw", width: "100%" }}>
         
         {/* Left logo */}
