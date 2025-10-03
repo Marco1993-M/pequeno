c72bb1f6426b358897c3e21319b84c2622dc418a
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-10-03 12:25:05 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -294,7 +294,7 @@ return (
 
 
         {/* CTA FINAL */}
-        <section className="w-full py-20 border-t border-gray-200">
+        <section className="w-full py-20">
           <div className="max-w-5xl mx-auto px-6 text-center">
             <h2 className="text-4xl font-bold mb-4">Have a Project in Mind?</h2>
             <p className="text-gray-700 max-w-xl mx-auto mb-6">
